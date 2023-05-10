ebbf51d0078afb82fa6c759be4289029e155be8f
vano2903 <davidevanoncini2003@gmail.com>
2023-05-10 22:08:35 +0200

    feat(controller): add user methods to controller (not complete)
    add method to check if a user already exists

---
+++ controller/user.go
@@ -0,0 +1,29 @@
+package controller
+
+import (
+	"context"
+
+	"github.com/ipaas-org/ipaas-backend/model"
+	"github.com/ipaas-org/ipaas-backend/repo"
+)
+
+func (c *Controller) UserDoesUserExist(ctx context.Context, user model.User) (bool, error) {
+	_, err := c.userRepo.FindByEmail(ctx, user.Email)
+	if err != nil {
+		if err == repo.ErrNotFound {
+			return false, nil
+		}
+		return false, err
+	}
+	return true, nil
+}
+
+func (c *Controller) UserCreateUser(ctx context.Context, user model.User) error {
+	c.l.Debugf("Creating user: %+v", user)
+	_, err := c.userRepo.InsertOne(ctx, &user)
+	return err
+}
+
+func (c *Controller) UserGetUser(ctx context.Context, email string) (*model.User, error) {
+	return c.userRepo.FindByEmail(ctx, email)
+}
