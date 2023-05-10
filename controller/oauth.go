f92f529d9e018c8966eaa1256dc4362e07da20b1
vano2903 <davidevanoncini2003@gmail.com>
2023-05-10 22:05:47 +0200

    feat(controller): add oauth methods in controller struct
    add method to generate redirectURI and retrive github user's information

---
+++ controller/oauth.go
@@ -0,0 +1,60 @@
+package controller
+
+import (
+	"context"
+	"time"
+
+	"github.com/google/uuid"
+	"github.com/ipaas-org/ipaas-backend/model"
+)
+
+func (c *Controller) createAndStoreState(ctx context.Context) (string, error) {
+	ran, err := uuid.NewRandom()
+	if err != nil {
+		return "", err
+	}
+	stateValue := ran.String()
+
+	id, err := c.stateRepo.Insert(ctx, &model.State{
+		State:          stateValue,
+		Issued:         time.Now(),
+		ExpirationDate: time.Now().Add(time.Minute * 5),
+	})
+	if err != nil {
+		c.l.Error("Error inserting state: %s", err.Error())
+		return "", err
+	}
+
+	c.l.Debug("Inserted state(%s) with id: %s", stateValue, id)
+	return stateValue, err
+}
+
+func (c *Controller) OauthGenerateLoginUri(ctx context.Context) string {
+	state, err := c.createAndStoreState(ctx)
+	if err != nil {
+		c.l.Error("Error creating state: %s", err.Error())
+		return ""
+	}
+
+	return c.oauthService.GenerateLoginRedirectUri(state)
+}
+
+func (c *Controller) OauthCheckState(ctx context.Context, state string) (bool, error) {
+	return c.stateRepo.DeleteByState(ctx, state)
+}
+
+func (c *Controller) OauthGetUser(ctx context.Context, code string) (model.User, error) {
+	accessToken, err := c.oauthService.GetAccessTokenFromCode(code)
+	if err != nil {
+		c.l.Error("Error getting access token from code: %s", err.Error())
+		return model.User{}, err
+	}
+
+	user, err := c.oauthService.GetUserInfo(accessToken)
+	if err != nil {
+		c.l.Error("Error getting user info: %s", err.Error())
+		return model.User{}, err
+	}
+
+	return user, nil
+}
