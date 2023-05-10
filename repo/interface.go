ae6eef0c18490f9871e6a27b9d547f8fbc4c7651
vano2903 <davidevanoncini2003@gmail.com>
2023-05-10 21:59:12 +0200

    feat(repo): add interface to define repositories
    add interface for user, token, state, application repo

--- repo/interface.go
+++ repo/interface.go
@@ -1,46 +1,46 @@
 package repo
 
 import (
+	"context"
+	"errors"
+
 	"github.com/ipaas-org/ipaas-backend/model"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
-// Interfaces ends with -er
 type (
-	//This interface has the methods declarations for the
-	//repo components.
 	UserRepoer interface {
-		GetByID(id string) (*model.User, error)
-		GetByStudentID(studentID int) (*model.User, error)
-		Insert(u *model.User) (id string, err error)
-		Update(u *model.User) error
-		DeleteById(id string) error
-		DeleteByStudentID(studentID int) error
-
-		GetBy(props map[string]interface{}) []*model.User
+		FindByID(ctx context.Context, id primitive.ObjectID) (*model.User, error)
+		FindByEmail(ctx context.Context, email string) (*model.User, error)
+		InsertOne(ctx context.Context, userModel *model.User) (interface{}, error)
+		UpdateGithubAccessTokenByID(ctx context.Context, githubAccessToken string, id primitive.ObjectID) (bool, error)
+		DeleteByID(ctx context.Context, id primitive.ObjectID) (bool, error)
 	}
 
 	TokenRepoer interface {
-		GetByToken(token string) (*model.RefreshToken, error)
-		Insert(t *model.RefreshToken) (id string, err error)
-		Delete(token string) error
-
-		GetBy(props map[string]interface{}) []*model.RefreshToken
+		FindByToken(ctx context.Context, token string) (*model.RefreshToken, error)
+		Insert(ctx context.Context, t *model.RefreshToken) (id interface{}, err error)
+		DeleteByToken(ctx context.Context, token string) (bool, error)
 	}
 
 	StateRepoer interface {
-		GetByState(state string) (*model.State, error)
-		Insert(s *model.State) (id string, err error)
-		Delete(state string) error
-
-		GetBy(props map[string]interface{}) []*model.State
+		FindByState(ctx context.Context, state string) (*model.State, error)
+		Insert(ctx context.Context, s *model.State) (id interface{}, err error)
+		DeleteByState(ctx context.Context, state string) (bool, error)
 	}
 
 	ApplicationRepoer interface {
-		Insert(a *model.Application) (id string, err error)
-		GetByContainerID(containerID string) (*model.Application, error)
-		Update(a *model.Application) error
-		Delete(containerID string) error
-
-		GetBy(props map[string]interface{}) []*model.Application
+		FindByContainerID(ctx context.Context, containerID string) (*model.Application, error)
+		FindByOwnerUsername(ctx context.Context, ownerUsername string) ([]*model.Application, error)
+		FindByOwnerUsernameAndTypeAndIsPublicTrue(ctx context.Context, ownerUsername string, appType string) ([]*model.Application, error)
+		FindByOwnerUsernameAndTypeAndIsPublicFalse(ctx context.Context, ownerUsername string, appType string) ([]*model.Application, error)
+		FindByOwnerUsernameAndIsUpdatableTrue(ctx context.Context, ownerUsername string) ([]*model.Application, error)
+		Insert(ctx context.Context, a *model.Application) (id interface{}, err error)
+		UpdateByID(ctx context.Context, a *model.Application, id primitive.ObjectID) (bool, error)
+		DeleteByID(ctx context.Context, id primitive.ObjectID) (bool, error)
 	}
+)
+
+var (
+	ErrNotFound = errors.New("not found")
 )
