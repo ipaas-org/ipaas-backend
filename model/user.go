4bf80bf0340f3d1604a17ecd3d9a4bc18625d766
vano2903 <davidevanoncini2003@gmail.com>
2023-05-10 21:50:32 +0200

    refactor(model): change user struct to fit better the github oauth informations

--- model/user.go
+++ model/user.go
@@ -2,20 +2,14 @@ package model
 
 import "go.mongodb.org/mongo-driver/bson/primitive"
 
-type User struct {
-	ID            primitive.ObjectID `bson:"_id" json:"_"`
-	StudentNumber int                `bson:"studentID" json:"matricola"` //id of the student (teachers will use the same, but it's 6 digit long instead of 5)
-	Name          string             `bson:"name" json:"nome"`
-	LastName      string             `bson:"lastName" json:"cognome"`
-	Email         string             `bson:"email" json:"email"`
-	Pfp           string             `bson:"pfp" json:"pfp"`
-}
-
-// !currently unused
-type StudentInfo struct {
-	Class               string `json:"classe"`
-	Year                int    `json:"anno"`
-	Field               string `json:"indirizzo"`
-	IsClassPresident    bool   `json:"rappresentante_classe"`
-	IsIstiturePresident bool   `json:"rappresentante_istituto"`
-}
+type (
+	User struct {
+		ID                primitive.ObjectID `bson:"_id" json:"-"`
+		Username          string             `bson:"username" json:"username"`   //github oauth field: login
+		FullName          string             `bson:"name" json:"name"`           //github oauth field: name
+		Email             string             `bson:"email" json:"email"`         //github oauth field: email primary
+		Pfp               string             `bson:"pfp" json:"pfp"`             //github oauth field: avatar_url
+		GithubUrl         string             `bson:"githubUrl" json:"githubUrl"` //github oauth field: url
+		GithubAccessToken string             `bson:"githubAccessToken" json:"githubAccessToken"`
+	}
+)
