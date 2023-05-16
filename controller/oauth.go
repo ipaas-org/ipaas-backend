e879104b5878634dd273eaaba55a78b7c83d33a5
vano2903 <davidevanoncini2003@gmail.com>
2023-05-16 22:13:06 +0200

    refactor(controller): add f print type to missing log print statements

--- controller/oauth.go
+++ controller/oauth.go
@@ -21,18 +21,18 @@ func (c *Controller) createAndStoreState(ctx context.Context) (string, error) {
 		ExpirationDate: time.Now().Add(time.Minute * 5),
 	})
 	if err != nil {
-		c.l.Error("Error inserting state: %s", err.Error())
+		c.l.Errorf("Error inserting state: %s", err.Error())
 		return "", err
 	}
 
-	c.l.Debug("Inserted state(%s) with id: %s", stateValue, id)
+	c.l.Debugf("Inserted state(%s) with id: %s", stateValue, id)
 	return stateValue, err
 }
 
 func (c *Controller) GenerateLoginUri(ctx context.Context) string {
 	state, err := c.createAndStoreState(ctx)
 	if err != nil {
-		c.l.Error("Error creating state: %s", err.Error())
+		c.l.Errorf("Error creating state: %s", err.Error())
 		return ""
 	}
 
@@ -46,13 +46,13 @@ func (c *Controller) CheckState(ctx context.Context, state string) (bool, error)
 func (c *Controller) GetUserFromOauthCode(ctx context.Context, code string) (model.User, error) {
 	accessToken, err := c.oauthService.GetAccessTokenFromCode(code)
 	if err != nil {
-		c.l.Error("Error getting access token from code: %s", err.Error())
+		c.l.Errorf("Error getting access token from code: %s", err.Error())
 		return model.User{}, err
 	}
 
 	user, err := c.oauthService.GetUserInfo(accessToken)
 	if err != nil {
-		c.l.Error("Error getting user info: %s", err.Error())
+		c.l.Errorf("Error getting user info: %s", err.Error())
 		return model.User{}, err
 	}
 
