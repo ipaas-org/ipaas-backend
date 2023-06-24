888e55572d46b3e83e271d36cb4e244b456e1636
vano2903 <davidevanoncini2003@gmail.com>
2023-06-24 19:23:37 +0200

    refactor(controller): split methods to increase single responability of methods

--- controller/oauth.go
+++ controller/oauth.go
@@ -8,39 +8,45 @@ import (
 	"github.com/ipaas-org/ipaas-backend/model"
 )
 
-func (c *Controller) createAndStoreState(ctx context.Context) (string, error) {
+func (c *Controller) CreateState() (string, error) {
 	ran, err := uuid.NewRandom()
 	if err != nil {
 		return "", err
 	}
-	stateValue := ran.String()
+	return ran.String(), nil
+}
 
+func (c *Controller) InsertState(ctx context.Context, state string) error {
 	id, err := c.stateRepo.Insert(ctx, &model.State{
-		State:          stateValue,
+		State:          state,
 		Issued:         time.Now(),
 		ExpirationDate: time.Now().Add(time.Minute * 5),
 	})
 	if err != nil {
 		c.l.Errorf("Error inserting state: %s", err.Error())
-		return "", err
+		return err
 	}
 
-	c.l.Debugf("Inserted state(%s) with id: %s", stateValue, id)
-	return stateValue, err
-}
-
-func (c *Controller) GenerateLoginUri(ctx context.Context) string {
-	state, err := c.createAndStoreState(ctx)
-	if err != nil {
-		c.l.Errorf("Error creating state: %s", err.Error())
-		return ""
-	}
-
-	return c.oauthService.GenerateLoginRedirectUri(state)
+	c.l.Debugf("Inserted state(%s) with id: %s", state, id)
+	return err
 }
 
 func (c *Controller) CheckState(ctx context.Context, state string) (bool, error) {
 	return c.stateRepo.DeleteByState(ctx, state)
+}
+
+func (c *Controller) GenerateLoginUri(ctx context.Context) string {
+	state, err := c.CreateState()
+	if err != nil {
+		c.l.Errorf("Error creating state: %s", err.Error())
+		return ""
+	}
+	if err := c.InsertState(ctx, state); err != nil {
+		c.l.Errorf("Error storing state: %s", err.Error())
+		return ""
+	}
+
+	return c.oauthService.GenerateLoginRedirectUri(state)
 }
 
 func (c *Controller) GetUserFromOauthCode(ctx context.Context, code string) (model.User, error) {
