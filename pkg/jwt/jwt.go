35ef5a4946e75fd48de2f2f46514df0da088aac4
vano2903 <davidevanoncini2003@gmail.com>
2023-10-18 11:18:46 +0200

    fix: change user email to user code in jwt and allow to specify duration in the jwt factory

--- pkg/jwt/jwt.go
+++ pkg/jwt/jwt.go
@@ -1,41 +1,42 @@
 package jwt
 
 import (
+	"strings"
 	"time"
 
 	"github.com/golang-jwt/jwt"
 )
 
 const (
-	_DEFAULT_EXPIRATION_TIME = 15 * time.Minute
+	DefaultExpirationTime = 15 * time.Minute
 )
 
 type JWThandler struct {
-	secret         string
+	secret         []byte
 	issuer         string
 	expirationTime time.Duration
 }
 
 type JWTClaims struct {
 	jwt.StandardClaims
-	UserEmail string `json:"user_email"`
+	UserCode string `json:"userCode"`
 }
 
 func NewJWThandler(secret, issuer string, expirationTime ...time.Duration) *JWThandler {
-	expTime := _DEFAULT_EXPIRATION_TIME
+	expTime := DefaultExpirationTime
 	if len(expirationTime) > 0 {
 		expTime = expirationTime[0]
 	}
 	return &JWThandler{
-		secret:         secret,
+		secret:         []byte(secret),
 		issuer:         issuer,
 		expirationTime: expTime,
 	}
 }
 
-func (j *JWThandler) GenerateToken(userEmail string) (string, error) {
+func (j *JWThandler) GenerateToken(userCode string) (string, time.Time, error) {
 	claims := JWTClaims{
-		UserEmail: userEmail,
+		UserCode: userCode,
 		StandardClaims: jwt.StandardClaims{
 			ExpiresAt: time.Now().Add(j.expirationTime).Unix(),
 			Issuer:    j.issuer,
@@ -43,13 +44,16 @@ func (j *JWThandler) GenerateToken(userEmail string) (string, error) {
 		},
 	}
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
-	return token.SignedString([]byte(j.secret))
+	signedToken, err := token.SignedString([]byte(j.secret))
+
+	expires := time.Unix(claims.ExpiresAt, 0)
+	return signedToken, expires, err
 }
 
 func (j *JWThandler) ValidateToken(tokenString string) (*JWTClaims, error) {
 	token, err := jwt.ParseWithClaims(tokenString, &JWTClaims{},
 		func(token *jwt.Token) (interface{}, error) {
-			return []byte(j.secret), nil
+			return j.secret, nil
 		})
 	if err != nil {
 		return nil, err
@@ -64,6 +68,9 @@ func (j *JWThandler) ValidateToken(tokenString string) (*JWTClaims, error) {
 func (j *JWThandler) IsTokenExpired(tokenString string) (bool, error) {
 	claims, err := j.ValidateToken(tokenString)
 	if err != nil {
+		if strings.HasPrefix(err.Error(), "token is expired by") {
+			return true, nil
+		}
 		return false, err
 	}
 	return j.IsTokenExpiredFromClaims(claims), nil
