8f49e38deb422d62a4fe927e78bdfae01ba77ddf
vano2903 <davidevanoncini2003@gmail.com>
2023-10-18 11:17:40 +0200

    fix: separate user info to different struct and store only important information on the user structure

--- model/user.go
+++ model/user.go
@@ -1,6 +1,10 @@
 package model
 
-import "go.mongodb.org/mongo-driver/bson/primitive"
+import (
+	"fmt"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
 
 const (
 	RoleUser    = "user"
@@ -9,19 +13,29 @@ const (
 
 type (
 	User struct {
-		ID                primitive.ObjectID `bson:"_id,omitemtpy" json:"-"`
-		Username          string             `bson:"username" json:"username"`   //github oauth field: login
-		FullName          string             `bson:"name" json:"name"`           //github oauth field: name
-		Email             string             `bson:"email" json:"email"`         //github oauth field: email primary
-		Pfp               string             `bson:"pfp" json:"pfp"`             //github oauth field: avatar_url
-		GithubUrl         string             `bson:"githubUrl" json:"githubUrl"` //github oauth field: url
-		GithubAccessToken string             `bson:"githubAccessToken" json:"-"`
-		NetworkID         string             `bson:"networkId" json:"networkId"`
-		Role              string             `bson:"role" json:"role"` //defaults to "user"
-		UserSettings      UserSettings       `bson:"userSettings" json:"userSettings"`
+		ID           primitive.ObjectID `bson:"_id,omitemtpy" json:"-"`
+		Code         string             `bson:"code" json:"code"`
+		NetworkID    string             `bson:"networkId" json:"networkId"`
+		Role         string             `bson:"role" json:"role"` //defaults to "user"
+		UserSettings *UserSettings      `bson:"userSettings" json:"userSettings"`
+		Info         *UserInfo          `bson:"userInfo" json:"userInfo"`
+	}
+
+	UserInfo struct {
+		Username          string `bson:"username" json:"username"`   //github oauth field: login
+		FullName          string `bson:"name" json:"name"`           //github oauth field: name
+		Email             string `bson:"email" json:"email"`         //github oauth field: email primary
+		Pfp               string `bson:"pfp" json:"pfp"`             //github oauth field: avatar_url
+		GithubUrl         string `bson:"githubUrl" json:"githubUrl"` //github oauth field: url
+		GithubAccessToken string `bson:"githubAccessToken" json:"-"`
 	}
 
 	UserSettings struct {
 		Theme string `bson:"theme" json:"theme"`
 	}
 )
+
+func (u *User) String() string {
+	// return fmt.Sprintf("%s [ID=%s]", u.Username, u.ID.Hex())
+	return fmt.Sprintf("UID=%s [_ID=%s](username=%q name=%q email=%q)", u.Code, u.ID.Hex(), u.Info.Username, u.Info.FullName, u.Info.Email)
+}
