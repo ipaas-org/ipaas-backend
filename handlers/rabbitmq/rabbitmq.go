a487c12b733f58e7fbe24bef86deac2b62bdb286
vano2903 <davidevanoncini2003@gmail.com>
2023-10-18 11:04:24 +0200

    feat: add self recovery function and adaptation to the changes in image builder

--- handlers/rabbitmq/rabbitmq.go
+++ handlers/rabbitmq/rabbitmq.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+	"runtime/debug"
 
 	"github.com/ipaas-org/ipaas-backend/controller"
 	"github.com/ipaas-org/ipaas-backend/model"
@@ -12,16 +13,20 @@ import (
 )
 
 type RabbitMQ struct {
-	l             *logrus.Logger
-	Error         <-chan error
-	Connection    *amqp.Connection
-	Channel       *amqp.Channel
-	ResponseQueue amqp.Queue
-	Delivery      <-chan amqp.Delivery
-	uri           string
-	// requestQueueName  string
+	Connection        *amqp.Connection
+	Channel           *amqp.Channel
+	ResponseQueue     amqp.Queue
+	Delivery          <-chan amqp.Delivery
 	responseQueueName string
-	Controller        *controller.Controller
+	uri               string
+	// requestQueueName  string
+
+	l          *logrus.Logger
+	Controller *controller.Controller
+
+	Done     chan struct{}
+	Error    <-chan error
+	restarts int
 }
 
 func NewRabbitMQ(uri, requestQueue, responseQueue string, controller *controller.Controller, logger *logrus.Logger) *RabbitMQ {
@@ -31,6 +36,8 @@ func NewRabbitMQ(uri, requestQueue, responseQueue string, controller *controller
 		l:                 logger,
 		responseQueueName: responseQueue,
 		Controller:        controller,
+		Done:              make(chan struct{}),
+		restarts:          0,
 	}
 }
 
@@ -52,7 +59,7 @@ func (r *RabbitMQ) Connect() error {
 		return fmt.Errorf("r.Channel.Qos: %w", err)
 	}
 
-	q, err := r.Channel.QueueDeclare(
+	responseQueue, err := r.Channel.QueueDeclare(
 		r.responseQueueName, // name
 		true,                // durable
 		false,               // delete when unused
@@ -65,13 +72,13 @@ func (r *RabbitMQ) Connect() error {
 	}
 
 	r.Delivery, err = r.Channel.Consume(
-		q.Name, // queue
-		"",     // consumer
-		false,  // auto-ack
-		false,  // exclusive
-		false,  // no-local
-		false,  // no-wait
-		nil,    // args
+		responseQueue.Name, // queue
+		"",                 // consumer
+		false,              // auto-ack
+		false,              // exclusive
+		false,              // no-local
+		false,              // no-wait
+		nil,                // args
 	)
 	if err != nil {
 		return fmt.Errorf("r.Channel.Consume: %w", err)
@@ -92,7 +99,44 @@ func (r *RabbitMQ) Close() error {
 	return nil
 }
 
-func (r *RabbitMQ) Consume(ctx context.Context) {
+func (r *RabbitMQ) Start(ctx context.Context, ID int, routineMonitor chan int) {
+	defer func(restarts int) {
+		if err := r.Close(); err != nil {
+			r.l.Errorf("error closing connection with rmq: %v:", err)
+		}
+		r.l.Info("rabbitmq connection closed")
+		rec := recover()
+		r.l.Debug("recover:", rec)
+		if rec != nil {
+			r.l.Error("rabbitmq routine panic:", rec)
+			r.l.Error(string(debug.Stack()))
+		}
+		if ctx.Err() == nil {
+			//  && restarts <= 5 {
+			// 	if restarts > 0 {
+			// 		time.Sleep(3 * time.Second)
+			// 	}
+			routineMonitor <- ID
+		} else {
+			r.l.Infof("rabbitmq routine [ID=%d] not restarting", ID)
+			r.Done <- struct{}{}
+		}
+	}(r.restarts)
+
+	r.l.Infof("starting rabbitmq routine [ID=%d]", ID)
+	if err := r.Connect(); err != nil {
+		r.l.Error("r.Connect():", err)
+		r.restarts++
+		return
+	}
+
+	r.l.Infof("rabbitmq routine [ID=%d] connected", ID)
+
+	r.consume(ctx)
+	r.l.Info("rabbitmq done consuming")
+}
+
+func (r *RabbitMQ) consume(ctx context.Context) {
 	for {
 		select {
 		case <-ctx.Done():
@@ -102,37 +146,54 @@ func (r *RabbitMQ) Consume(ctx context.Context) {
 			r.l.Error(err)
 		case d := <-r.Delivery:
 			r.l.Info("received message from rabbitmq")
-			r.l.Debug(string(d.Body))
-
+			r.l.Debugf("received: %q", string(d.Body))
+			if d.Body == nil {
+				if err := d.Ack(false); err != nil {
+					r.l.Errorf("r.Consume.Ack(): %v:", err)
+					return
+				}
+				continue
+			}
 			response := new(model.BuildResponse)
 			if err := json.Unmarshal(d.Body, response); err != nil {
-				r.l.Error("r.Consume.json.Unmarshal(): %w:", err)
+				r.l.Errorf("r.Consume.json.Unmarshal(): %v:", err)
 				r.l.Debug(string(d.Body))
 				if err := d.Nack(false, false); err != nil {
-					r.l.Error("r.Consume.Nack(): %w:", err)
+					r.l.Errorf("r.Consume.Nack(): %v:", err)
+					return
 				}
+				continue
+				//TODO: should send to a unprocessable queue
 			}
 
 			r.l.Debug(response)
-			if response.Error != nil {
-				r.l.Error("r.Controller: error building image", response.Error.Message)
-				r.l.Debug(response.Error.Message)
+			if response.IsError {
+				r.l.Error("r.Controller: error building image", response.Message)
+				r.l.Error("r.Controller: error building image fault:", response.Fault)
+				if response.Fault == model.ResponseErrorFaultService {
+					//TODO: resend the message to the queue to process again, at least 3 times
+					//if it fails notify the user that the build failed and to try again later
+				} else {
+					//TODO: notify the user that the build failed and the reason
+					//build error is in response.Message
+				}
 				if err := d.Nack(false, false); err != nil {
-					r.l.Error("r.Consume.Nack(): %w:", err)
+					r.l.Errorf("r.Consume.Nack(): %v:", err)
+					return
 				}
 			}
 
-
-			
-			if err := r.Controller.CreateContainerFromIDAndImage(ctx, response.UUID, response.LatestCommit, imageID); err != nil {
-				r.l.Errorf("r.Controller.CreateContainerFromIDAndImage(): %w:", err)
+			if err := r.Controller.CreateContainerFromIDAndImage(ctx, response.UUID, response.BuiltCommit, response.ImageID); err != nil {
+				r.l.Errorf("r.Controller.CreateContainerFromIDAndImage(): %v:", err)
 				if err := d.Nack(false, false); err != nil {
-					r.l.Errorf("r.Consume.Nack(): %w:", err)
+					r.l.Errorf("r.Consume.Nack(): %v:", err)
+					return
 				}
 			}
 
 			if err := d.Ack(false); err != nil {
-				r.l.Error("r.Consume.Ack(): %w:", err)
+				r.l.Errorf("r.Consume.Ack(): %v:", err)
+				return
 			}
 			r.l.Info("acknowledged message from rabbitmq")
 		}
