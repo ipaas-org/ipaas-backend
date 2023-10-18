1035d6e85015fa374341dcc6bb96e49045096045
vano2903 <davidevanoncini2003@gmail.com>
2023-10-18 11:05:24 +0200

    fix: add _ instead of spaces in errors string

--- handlers/httpserver/errors.go
+++ handlers/httpserver/errors.go
@@ -2,17 +2,19 @@ package httpserver
 
 const (
 	//auth errors
-	ErrMissingAuthorizationHeader = "missing authorization header"
-	ErrInvalidAuthorizationHeader = "invalid authorization header"
-	ErrInvalidAccessToken         = "invalid access token"
-	ErrAccessTokenExpired         = "access token expired"
+	ErrMissingAuthorizationHeader = "missing_authorization_header"
+	ErrInvalidAuthorizationHeader = "invalid_authorization_header"
+	ErrInvalidAccessToken         = "invalid_access_token"
+	ErrAccessTokenExpired         = "access_token_expired"
 	ErrAccessTokenNotFound        = "access_token_not_found"
+	ErrInvalidRefreshToken        = "invalid_refresh_token"
+	ErrRefreshTokenExpired        = "refresh_token_expired"
 
 	//state errors
 	ErrInvalidState = "invalid_state"
 
 	//user related errors
-	ErrInvalidUser                      = "invalid user"
+	ErrInvalidUser                      = "invalid_user"
 	ErrUserNotFound                     = "user_not_found"
 	ErrNameAlreadyTaken                 = "name_already_taken"
 	ErrUnableToBuildImageInCurrentState = "unable_to_build_image_in_current_state"
