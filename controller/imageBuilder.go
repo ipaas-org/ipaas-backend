4abb1adea9fb53aeda3a0633b0526782a06f864b
vano2903 <davidevanoncini2003@gmail.com>
2023-10-18 10:54:32 +0200

    refactor: change app fields from status to state

--- controller/imageBuilder.go
+++ controller/imageBuilder.go
@@ -10,14 +10,14 @@ import (
 // TODO: IMPORTANTE l'user id quando si crea l'immagine non può essere la mail, meglio usare l'id dell'utente o il suo username
 // TODO: non accettare richieste di build image di un applicazione se l'applicazione è già in status pending|updating
 func (c *Controller) BuildImage(ctx context.Context, app *model.Application, providerToken string) error {
-	if app.Status == StatusPending || app.Status == StatusBuilding || app.Status == StatusUpdating {
+	if app.State == StatePending || app.State == StateBuilding || app.State == StateUpdating {
 		return ErrUnableToBuildImageInCurrentState
 	}
 
 	request := model.BuildRequest{
 		UUID:      app.ID.Hex(),
 		Token:     providerToken,
-		UserID:    app.OwnerEmail,
+		UserID:    app.Owner,
 		Type:      "repo",
 		Connector: "github",
 		Repo:      app.GithubRepo,
@@ -26,14 +26,14 @@ func (c *Controller) BuildImage(ctx context.Context, app *model.Application, pro
 
 	if err := c.imageBuilder.BuildImage(request); err != nil {
 		c.l.Errorf("error building image: %v", err)
-		app.Status = StatusFailed
+		app.State = StateFailed
 		if err := c.UpdateApplicationState(ctx, app); err != nil {
 			return fmt.Errorf("c.updateApplicationState: %w", err)
 		}
 		return fmt.Errorf("c.imageBuilder.BuildImage: %w", err)
 	}
 
-	app.Status = StatusBuilding
+	app.State = StateBuilding
 	if err := c.UpdateApplicationState(ctx, app); err != nil {
 		return fmt.Errorf("c.updateApplicationState: %w", err)
 	}
