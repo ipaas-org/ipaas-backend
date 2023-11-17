ee26d32db15fe9249e06552d60b5a11c7b3b89ad
vano2903 <davidevanoncini2003@gmail.com>
2023-11-17 15:16:22 +0100

    feat: add create web application that sets the application in the database and add create application from application id and image id that builds the container itself

--- controller/application.go
+++ controller/application.go
@@ -3,20 +3,11 @@ package controller
 import (
 	"context"
 	"fmt"
+	"time"
 
 	"github.com/ipaas-org/ipaas-backend/model"
 	"github.com/ipaas-org/ipaas-backend/repo"
 	"go.mongodb.org/mongo-driver/bson/primitive"
-)
-
-const (
-	StateCreated  = "created"
-	StatePending  = "pending"
-	StateStarting = "starting"
-	StateRunning  = "running"
-	StateFailed   = "failed"
-	StateBuilding = "building"
-	StateUpdating = "updating"
 )
 
 // todo: is not available when there is a web applpication with the same name
@@ -36,13 +27,13 @@ func (c *Controller) IsNameAvailableUserWide(ctx context.Context, name, username
 	return available
 }
 
-func (c *Controller) DoesApplicationExists(ctx context.Context, app *model.Application) (bool, error) {
-	_, err := c.ApplicationRepo.FindByID(ctx, app.ID)
+func (c *Controller) DoesApplicationExists(ctx context.Context, applicationID primitive.ObjectID) (bool, error) {
+	_, err := c.ApplicationRepo.FindByID(ctx, applicationID)
 	if err != nil {
 		if err == repo.ErrNotFound {
 			return false, nil
 		}
-		return false, fmt.Errorf("c.applicationRepo.FindByID: %w", err)
+		return false, err
 	}
 	return true, nil
 }
@@ -52,7 +43,6 @@ func (c *Controller) GetApplicationByID(ctx context.Context, id primitive.Object
 }
 
 func (c *Controller) InsertApplication(ctx context.Context, app *model.Application) error {
-	app.State = StateCreated
 	app.ID = primitive.NewObjectID()
 	_, err := c.ApplicationRepo.InsertOne(ctx, app)
 	if err != nil {
@@ -61,22 +51,109 @@ func (c *Controller) InsertApplication(ctx context.Context, app *model.Applicati
 	return nil
 }
 
-func (c *Controller) UpdateApplicationState(ctx context.Context, app *model.Application) error {
+func (c *Controller) UpdateApplication(ctx context.Context, app *model.Application) error {
 	if _, err := c.ApplicationRepo.UpdateByID(ctx, app, app.ID); err != nil {
-		c.l.Errorf("error updating application status: %v", err)
-		return fmt.Errorf("c.applicationRepo.UpdateByID: %w", err)
+		c.l.Errorf("error updating application: %v", err)
+		return err
 	}
 	return nil
 }
 
-// TODO: check if this function is actually needed or if it's better to just insert and call BuildImage
-func (c *Controller) TOCHECKCreateNewApplication(ctx context.Context, app *model.Application, providerAccessToken string) error {
+// this function will insert a new application and send the build request to image builder
+func (c *Controller) CreateNewWebApplication(ctx context.Context, userCode, providerAccessToken, name, gitRepo, gitBranch, listeningPort string, envs []model.KeyValue) (*model.Application, error) {
+	app := new(model.Application)
+	app.Name = name
+	app.Kind = model.ApplicationKindWeb
+	app.State = model.ApplicationStatePending
+	app.CreatedAt = time.Now()
+	app.Owner = userCode
+	app.IsPublic = true //! for now all applications are public
+	app.IsUpdatable = false
+	app.ListeningPort = listeningPort
+	app.GithubBranch = gitBranch
+	app.GithubRepo = gitRepo
+	app.Envs = envs
+
 	if err := c.InsertApplication(ctx, app); err != nil {
-		return fmt.Errorf("c.InsertApplication: %w", err)
+		c.l.Errorf("error inserting application: %v", err)
+		return nil, err
 	}
 
 	if err := c.BuildImage(ctx, app, providerAccessToken); err != nil {
-		return fmt.Errorf("c.BuildImage: %w", err)
+		c.l.Errorf("error building image: %v", err)
+		return nil, err
+	}
+	return app, nil
+}
+
+func (c *Controller) CreateApplicationFromApplicationIDandImageID(ctx context.Context, applicationID, imageID, BuildCommit string) error {
+	//convert the app id to primitive object id
+	appID, err := primitive.ObjectIDFromHex(applicationID)
+	if err != nil {
+		c.l.Errorf("error converting application id to primitive, this error should not happen: %v", err)
+		return err
+	}
+
+	//get application from application id
+	app, err := c.ApplicationRepo.FindByID(ctx, appID)
+	if err != nil {
+		//!if this error happens it means that the application was not created succesfully or was deleted during the build process
+		//todo: it should not happen, the user can not delete the application while building for now
+		//todo: when implemented correctly the user will be able to stop the building process and delete the application
+		c.l.Errorf("error getting application from application id: %v", err)
+		return err
+	}
+	//update status of application to starting and set the built commit to builtCommit
+	app.State = model.ApplicationStateStarting
+	app.BuiltCommit = BuildCommit
+	if _, err := c.ApplicationRepo.UpdateByID(ctx, app, app.ID); err != nil {
+		c.l.Errorf("error updating application: %v", err)
+		return err
+	}
+
+	//get user from the application owner and get his docker network id
+	user, err := c.UserRepo.FindByCode(ctx, app.Owner)
+	if err != nil {
+		c.l.Errorf("error user not found for application owner: %v", err)
+		return err
+	}
+
+	//start a container from the application
+	labels := c.GenerateLabels(app.Name, app.Owner, app.Kind)
+	host := fmt.Sprintf("%s.localhost", app.Name)
+	traefikLabels := c.GenerateTraefikDnsLables(app.Name, host, app.ListeningPort)
+	labels = append(labels, traefikLabels...)
+	container, err := c.serviceManager.CreateNewContainer(ctx, app.Name, imageID, app.Envs, labels)
+	if err != nil {
+		c.l.Errorf("error creating new container: %v", err)
+		app.State = model.ApplicationStateFailed
+		if _, err := c.ApplicationRepo.UpdateByID(ctx, app, app.ID); err != nil {
+			c.l.Errorf("error updating application: %v", err)
+		}
+		return err
+	}
+	//connect container to user's network id and set as dns the application name
+	c.serviceManager.ConnectContainerToNetwork(ctx, container.ContainerID, user.NetworkID, app.Name)
+	//todo: add traefik network id (it should be in the config file for now, in the future it will be a dns service)
+	//for now traefik is connected to host network so it's fine
+
+	//start the container
+	if err := c.serviceManager.StartContainerByID(ctx, container.ContainerID); err != nil {
+		c.l.Errorf("error starting container: %v", err)
+		app.State = model.ApplicationStateFailed
+		if _, err := c.ApplicationRepo.UpdateByID(ctx, app, app.ID); err != nil {
+			c.l.Errorf("error updating application: %v", err)
+		}
+		return err
+	}
+
+	//update the application with the container informations and status running
+	app.State = model.ApplicationStateRunning
+	app.Container = container
+	app.DnsName = host
+	if _, err := c.ApplicationRepo.UpdateByID(ctx, app, app.ID); err != nil {
+		c.l.Errorf("error updating application: %v", err)
+		return err
 	}
 	return nil
 }
