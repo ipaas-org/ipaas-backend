6c96110b38b34ebd0383daf46cb18f51e9e404fc
vano2903 <davidevanoncini2003@gmail.com>
2023-11-17 15:03:48 +0100

    feat: add an error type for http

--- handlers/httpserver/errors.go
+++ handlers/httpserver/errors.go
@@ -1,25 +1,31 @@
 package httpserver
 
+type HttpErrorType string
+
 const (
 	//auth errors
-	ErrMissingAuthorizationHeader = "missing_authorization_header"
-	ErrInvalidAuthorizationHeader = "invalid_authorization_header"
-	ErrInvalidAccessToken         = "invalid_access_token"
-	ErrAccessTokenExpired         = "access_token_expired"
-	ErrAccessTokenNotFound        = "access_token_not_found"
-	ErrInvalidRefreshToken        = "invalid_refresh_token"
-	ErrRefreshTokenExpired        = "refresh_token_expired"
+	ErrMissingAuthorizationHeader HttpErrorType = "missing_authorization_header"
+	ErrInvalidAuthorizationHeader HttpErrorType = "invalid_authorization_header"
+	ErrInvalidAccessToken         HttpErrorType = "invalid_access_token"
+	ErrAccessTokenExpired         HttpErrorType = "access_token_expired"
+	ErrAccessTokenNotFound        HttpErrorType = "access_token_not_found"
+	ErrInvalidRefreshToken        HttpErrorType = "invalid_refresh_token"
+	ErrRefreshTokenExpired        HttpErrorType = "refresh_token_expired"
 
 	//state errors
-	ErrInvalidState = "invalid_state"
+	ErrInvalidState HttpErrorType = "invalid_state"
 
 	//user related errors
-	ErrInvalidUser                      = "invalid_user"
-	ErrUserNotFound                     = "user_not_found"
-	ErrNameAlreadyTaken                 = "name_already_taken"
-	ErrUnableToBuildImageInCurrentState = "unable_to_build_image_in_current_state"
+	ErrInvalidUser                      HttpErrorType = "invalid_user"
+	ErrUserNotFound                     HttpErrorType = "user_not_found"
+	ErrNameAlreadyTaken                 HttpErrorType = "name_already_taken"
+	ErrUnableToBuildImageInCurrentState HttpErrorType = "unable_to_build_image_in_current_state"
+
+	// application related errors
+	ErrNameTaken HttpErrorType = "name_taken"
 
 	//http errors
-	ErrInvalidRequestBody = "invalid_request_body"
-	ErrUnexpected         = "unexpected_error"
+	ErrInvalidRequestBody HttpErrorType = "invalid_request_body"
+	ErrUnexpected         HttpErrorType = "unexpected_error"
+	ErrNotImplemented     HttpErrorType = "not_implemented"
 )
