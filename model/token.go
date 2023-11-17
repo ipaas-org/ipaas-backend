fb833ac4e0787d707ec918bed73369eaf0333d84
vano2903 <davidevanoncini2003@gmail.com>
2023-11-17 14:54:03 +0100

    feat: add accessToken type

--- model/token.go
+++ model/token.go
@@ -7,8 +7,13 @@ import (
 )
 
 type RefreshToken struct {
-	ID         primitive.ObjectID `bson:"_id,omitempty"`
-	UserCode   string             `bson:"userCode"`
-	Token      string             `bson:"token"`
-	Expiration time.Time          `bson:"expiration"`
+	ID        primitive.ObjectID `bson:"_id,omitempty"`
+	UserCode  string             `bson:"userCode"`
+	Token     string             `bson:"token"`
+	ExpiresAt time.Time          `bson:"expiresAt"`
+}
+
+type AccessToken struct {
+	Token     string    `json:"token"`
+	ExpiresAt time.Time `json:"expiresAt"`
 }
