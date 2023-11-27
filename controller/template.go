79f431461ac88c301859f31c8d715be6fe2de484
vano2903 <davidevanoncini2003@gmail.com>
2023-11-27 16:35:10 +0100

    feat: add template functionalities

---
+++ controller/template.go
@@ -0,0 +1,87 @@
+package controller
+
+import (
+	"context"
+	"time"
+
+	"github.com/ipaas-org/ipaas-backend/model"
+	"github.com/ipaas-org/ipaas-backend/repo"
+)
+
+// todo: use this function to check if the name is available for a database
+func (c *Controller) IsNameAvailableUserWide(ctx context.Context, name, userCode string) bool {
+	_, err := c.ApplicationRepo.FindByNameAndOwner(ctx, name, userCode)
+	available := err == repo.ErrNotFound
+	c.l.Debugf("is name[%s] available for %s: %t", name, userCode, available)
+	return available
+}
+
+func (c *Controller) CreateNewApplicationBasedOnTemplate(ctx context.Context, userCode, name string, template *model.Template, envs []model.KeyValue) (*model.Application, error) {
+	c.l.Debugf("creating a new application for %s based on template %s", userCode, template.Code)
+	app := new(model.Application)
+	app.Name = name
+	app.Kind = template.Kind
+	app.State = model.ApplicationStateStarting
+	app.CreatedAt = time.Now()
+	app.Owner = userCode
+	app.IsPublic = false
+	app.IsUpdatable = false
+	app.ListeningPort = template.ListeningPort
+	app.Envs = envs
+	app.BasedOn = template.Code
+
+	for _, te := range template.RequiredEnvs {
+		var found bool
+		for _, e := range envs {
+			if e.Key == te.Key {
+				found = true
+				break
+			}
+		}
+		if !found {
+			return nil, ErrMissingRequiredEnvForTemplate
+		}
+	}
+
+	user, err := c.UserRepo.FindByCode(ctx, userCode)
+	if err != nil {
+		c.l.Errorf("error finding user by code: %v", err)
+		return nil, err
+	}
+
+	if err := c.InsertApplication(ctx, app); err != nil {
+		c.l.Errorf("error inserting application: %v", err)
+		return nil, err
+	}
+
+	labels := c.GenerateLabels(name, userCode, app.Kind)
+
+	container, err := c.createConnectAndStartContainer(ctx, name, template.ImageID, user.NetworkID, envs, labels)
+	if err != nil {
+		c.l.Errorf("error creating container: %v", err)
+		app.State = model.ApplicationStateFailed
+		if _, err := c.ApplicationRepo.UpdateByID(ctx, app, app.ID); err != nil {
+			c.l.Errorf("error updating application: %v", err)
+		}
+		return nil, err
+	}
+
+	app.State = model.ApplicationStateRunning
+	app.Container = container
+	app.DnsName = name
+	if _, err := c.ApplicationRepo.UpdateByID(ctx, app, app.ID); err != nil {
+		c.l.Errorf("error updating application: %v", err)
+		return nil, err
+	}
+
+	return app, nil
+}
+
+func (c *Controller) GetTemplateByCode(ctx context.Context, code string) (*model.Template, error) {
+	template, err := c.TemplateRepo.FindByCode(ctx, code)
+	if err != nil {
+		c.l.Errorf("error finding template by code %s: %v", code, err)
+		return nil, err
+	}
+	return template, nil
+}
