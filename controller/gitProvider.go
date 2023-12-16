a9716487784e730e04286a1b87d58d0364a42802
vano2903 <davidevanoncini2003@gmail.com>
2023-12-16 04:50:10 +0100

    feat: add validateGitRepo function, checks if a repo is valid and if it is returns default branch and branches

--- controller/gitProvider.go
+++ controller/gitProvider.go
@@ -11,3 +11,19 @@ func (c *Controller) GetAvailableGitRepos(ctx context.Context, accessToken strin
 	// repos, err := c.gitProvider.GetAvailableRepos(ctx, accessToken)
 	return nil, ErrNotImplemented
 }
+
+// repo is name/repo
+func (c *Controller) ValidateGitRepo(ctx context.Context, user *model.User, repo string) (string, []string, error) {
+	// return c.gitProvider.ValidateRepo(ctx, accessToken, username, repo)
+	username, repo, err := c.gitProvider.GetUserAndRepo(repo)
+	if err != nil {
+		return "", nil, err
+	}
+
+	defaultBranch, branches, err := c.gitProvider.GetRepoBranches(user.Info.GithubAccessToken, username, repo)
+	if err != nil {
+		c.l.Errorf("error getting branches from git provider: %v", err)
+		return "", nil, err
+	}
+	return defaultBranch, branches, nil
+}
