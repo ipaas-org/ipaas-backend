f324491a7f5e671a3f90d501b56d067872dc732d
vano2903 <davidevanoncini2003@gmail.com>
2024-03-11 10:18:17 +0100

    feat: add template model

--- model/template.go
+++ model/template.go
@@ -1,20 +1,20 @@
 package model
 
 type Template struct {
-	Code            string      `bson:"code" json:"code"`
-	Available       bool        `bson:"available" json:"available"`
-	Name            string      `bson:"name" json:"name"`
-	ImageName       string      `bson:"imageName" json:"-"`
-	ImageID         string      `bson:"imageID" json:"-"`
-	ListeningPort   string      `bson:"listeningPort" json:"-"`
-	RequiredEnvs    []KeyValue  `bson:"requiredEnvs" json:"requiredEnvs"`
-	OptionalEnvs    []KeyValue  `bson:"optionalEnvs" json:"optionalEnvs"`
-	DefaultEnvs     []KeyValue  `bson:"defaultEnvs" json:"-"`
-	IsUpdatabale    bool        `bson:"isUpdatabale" json:"-"`
-	Description     string      `bson:"description" json:"description"`
-	Documentation   string      `bson:"documentation" json:"documentation"`
-	PersistancePath string      `bson:"persistancePath" json:"-"`
-	Kind            ServiceKind `bson:"kind" json:"kind"`
+	Code            string          `bson:"code" json:"code"`
+	Available       bool            `bson:"available" json:"available"`
+	Name            string          `bson:"name" json:"name"`
+	ImageName       string          `bson:"imageName" json:"-"`
+	ImageID         string          `bson:"imageID" json:"-"`
+	ListeningPort   string          `bson:"listeningPort" json:"-"`
+	RequiredEnvs    []KeyValue      `bson:"requiredEnvs" json:"requiredEnvs"`
+	OptionalEnvs    []KeyValue      `bson:"optionalEnvs" json:"optionalEnvs"`
+	DefaultEnvs     []KeyValue      `bson:"defaultEnvs" json:"-"`
+	IsUpdatabale    bool            `bson:"isUpdatabale" json:"-"`
+	Description     string          `bson:"description" json:"description"`
+	Documentation   string          `bson:"documentation" json:"documentation"`
+	PersistancePath string          `bson:"persistancePath" json:"-"`
+	Kind            ApplicationKind `bson:"kind" json:"kind"`
 }
 
 /*
