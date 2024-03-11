bd13b4d66cdcadcac0fff7ba1bf3670452d25641
vano2903 <davidevanoncini2003@gmail.com>
2024-03-11 10:28:32 +0100

    wip: update event watcher to work with k8s

--- handlers/containerEvents/events.go
+++ handlers/containerEvents/events.go
@@ -4,10 +4,8 @@ import (
 	"context"
 	"runtime/debug"
 
-	"github.com/docker/docker/api/types"
 	"github.com/docker/docker/client"
 	"github.com/ipaas-org/ipaas-backend/controller"
-	"github.com/ipaas-org/ipaas-backend/model"
 	"github.com/sirupsen/logrus"
 )
 
@@ -59,68 +57,69 @@ func StartConainerEventHandler(ctx context.Context, c *ContainerEventHandler, ID
 		logger.Errorf("error creating ContainerEventHandler: %v", err)
 		return
 	}
-	c.start(ctx)
+	c.l.Warn("Event handler not implemented")
+	// c.start(ctx)
 }
 
-func (c *ContainerEventHandler) start(ctx context.Context) {
-	eventChan, errChan := c.cli.Events(ctx, types.EventsOptions{})
-	for {
-		select {
-		case <-ctx.Done():
-			c.l.Info("ContainerEventHandler context canceled, stopping")
-			return
+// func (c *ContainerEventHandler) start(ctx context.Context) {
+// 	eventChan, errChan := c.cli.Events(ctx, types.EventsOptions{})
+// 	for {
+// 		select {
+// 		case <-ctx.Done():
+// 			c.l.Info("ContainerEventHandler context canceled, stopping")
+// 			return
 
-		case event := <-eventChan:
-			//log.Println(event)
-			switch event.Type {
-			case "container":
-				//log.Println("container event\n\n")
-				switch event.Action {
-				case "die", "kill":
-					c.l.Infof("[EVENT] Container %s: %s", event.Action, event.Actor.ID)
-					app, err := c.controller.ApplicationRepo.FindByContainerID(ctx, event.Actor.ID)
-					if err != nil {
-						c.l.Infof("a container died or was killed but it's not handled by ipaas: %v", err)
-						continue
-					}
-					if app.State == model.ApplicationStateDeleting {
-						c.l.Info("application is being deleted, passing")
-						continue
-					}
-					c.l.Info("unexpected container death, notifying user")
-					app.State = model.ApplicationStateCrashed
-					if _, err := c.controller.ApplicationRepo.UpdateByID(ctx, app, app.ID); err != nil {
-						c.l.Errorf("error updating application: %v", err)
-						continue
-					}
-				//todo: notify user
-				// case "health_status":
-				// 	log.Println("[EVENT] Container health status:", event.Actor.ID)
-				case "start":
-					c.l.Infof("[EVENT] Container %s: %s", event.Action, event.Actor.ID)
-					app, err := c.controller.ApplicationRepo.FindByContainerID(ctx, event.Actor.ID)
-					if err != nil {
-						c.l.Infof("a container started but it's not handled by ipaas: %v", err)
-						continue
-					}
-					if app.State != model.ApplicationStateCrashed {
-						c.l.Info("application is not crashed, passing")
-						continue
-					}
-					c.l.Info("container started, updating state")
-					app.State = model.ApplicationStateRunning
-					if _, err := c.controller.ApplicationRepo.UpdateByID(ctx, app, app.ID); err != nil {
-						c.l.Errorf("error updating application: %v", err)
-						continue
-					}
+// 		case event := <-eventChan:
+// 			//log.Println(event)
+// 			switch event.Type {
+// 			case "container":
+// 				//log.Println("container event\n\n")
+// 				switch event.Action {
+// 				case "die", "kill":
+// 					c.l.Infof("[EVENT] Container %s: %s", event.Action, event.Actor.ID)
+// 					// app, err := c.controller.ApplicationRepo.FindByName(ctx, event.Actor.ID)
+// 					if err != nil {
+// 						c.l.Infof("a container died or was killed but it's not handled by ipaas: %v", err)
+// 						continue
+// 					}
+// 					if app.State == model.ApplicationStateDeleting {
+// 						c.l.Info("application is being deleted, passing")
+// 						continue
+// 					}
+// 					c.l.Info("unexpected container death, notifying user")
+// 					app.State = model.ApplicationStateCrashed
+// 					if _, err := c.controller.ApplicationRepo.UpdateByID(ctx, app, app.ID); err != nil {
+// 						c.l.Errorf("error updating application: %v", err)
+// 						continue
+// 					}
+// 				//todo: notify user
+// 				// case "health_status":
+// 				// 	log.Println("[EVENT] Container health status:", event.Actor.ID)
+// 				case "start":
+// 					c.l.Infof("[EVENT] Container %s: %s", event.Action, event.Actor.ID)
+// 					// app, err := c.controller.ApplicationRepo.FindByContainerID(ctx, event.Actor.ID)
+// 					if err != nil {
+// 						c.l.Infof("a container started but it's not handled by ipaas: %v", err)
+// 						continue
+// 					}
+// 					if app.State != model.ApplicationStateCrashed {
+// 						c.l.Info("application is not crashed, passing")
+// 						continue
+// 					}
+// 					c.l.Info("container started, updating state")
+// 					app.State = model.ApplicationStateRunning
+// 					if _, err := c.controller.ApplicationRepo.UpdateByID(ctx, app, app.ID); err != nil {
+// 						c.l.Errorf("error updating application: %v", err)
+// 						continue
+// 					}
 
-					// default:
-					// 	c.l.Infof("[EVENT] Container %s: %s", event.Action, event.Actor.ID)
-				}
-			}
-		case err := <-errChan:
-			c.l.Errorf("error from docker event stream: %v", err)
+// 					// default:
+// 					// 	c.l.Infof("[EVENT] Container %s: %s", event.Action, event.Actor.ID)
+// 				}
+// 			}
+// 		case err := <-errChan:
+// 			c.l.Errorf("error from docker event stream: %v", err)
 
-		}
-	}
-}
+// 		}
+// 	}
+// }
