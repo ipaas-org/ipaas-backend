53211d07b2b397998c686869ac5a2476f2916c11
vano2903 <davidevanoncini2003@gmail.com>
2024-10-08 13:57:08 +0200

    feat: remove pull repo as it is not its concern anymore but image builder handles it

--- services/gitProvider/interface.go
+++ services/gitProvider/interface.go
@@ -18,8 +18,6 @@ type Provider interface {
 	GetUserInfo(ctx context.Context, accessToken string) (*model.UserInfo, error)
 
 	//*git functions
-	// returns the commit of the pulled repo
-	PullRepository(ctx context.Context, accessToken, username, repo, branch, path string) (string, error)
 	//given a repo like username/repo returns the username and the repo name
 	GetUserAndRepo(ctx context.Context, repo string) (string, string, error)
 	GetUserRepos(ctx context.Context, accessToken string) ([]model.GitRepo, error)
@@ -37,4 +35,5 @@ var (
 	ErrRateLimitReached error = errors.New("rate limit reached")
 	ErrRepoNotFound     error = errors.New("repo not found")
 	ErrNoCommitsFound   error = errors.New("no commits found")
+	ErrBranchNotFound   error = errors.New("branch not found")
 )
